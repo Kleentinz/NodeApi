17f3ffb0e010c444c29492e0fd192472946b600d
kleentinz <jubrilrasheed2020@gmail.com>
2023-11-26 14:45:22 +0100

    #update

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const Joi = require('joi')
+const Joi = require('joi');
+const logger = require('./logger');
+const login = require('./login');
 const express = require('express');
-const app = express ();
+const app = express();
+const helmet = require('helmet');
+const morgan = require ('morgan');
+
 // The app.use is called middleware
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded ({extend: true}));
+app.use(express.static('public'));
+app.use(helmet())
+app.use(login);
+app.use(logger);
+app.use(morgan('tiny'))
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+console.log(`app: ${app.get('env')}`)
+
+app.use(function (req, res, next){
+    console.log("Login...");
+    next();
+});
+
+app.use(function(req, res, next){
+    console.log("Authenticating")
+    next();
+});
 
 const courses =[
     {id:1, name:"html"},
@@ -31,15 +54,11 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res)=>{
-    const schema = {
-        name:Joi.string().min(3).required()
-    };
-    const result=Joi.validate(req.body,schema);
-    console.log(result)
-    if (result.error){
-        res.status(400).send(result.error.details[0].message)
-        return;
-    }
+    
+    const {error}= validateCourse(req.body);
+    
+    if (error) return res.status(400).send(error.details[0].message)
+        
     const course = {
         id : courses.length + 1 ,
         name: req.body.name
@@ -53,24 +72,34 @@ app.put('/api/courses/:id', (req, res)=>{
         parseInt(req.params.id));
         if(!course) res.status(404).send('the course with the id not found');
 
-        const schema = {
-            name:Joi.string().min(3).required()
-        };
-        const result=Joi.validate(req.body,schema);
+            const {error} = validateCourse(req.body);
         
-        if (result.error){
-            res.status(400).send(result.error.details[0].message)
-            return;
-        }
+            if (error) return res.status(400).send(error.details[0].message)
 
         course.name = req.body.name
         res.send(course)
         
 });
+
+app.delete('/api/courses/:id', (req, res) =>{
+    const course = courses.find(c=> c.id===parseInt(req.params.id));
+    if (!course) return res.status(404).send("the course with the id not found")
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course)
+})
 app.get('/api/course/:id', (req, res) =>{
     res.send(req.params.id)
 });
-
+function validateCourse(course) {
+    const schema = {
+      name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema)
+    }
+    
+  
 const port = process.env.PORT || 3000;
 
 app.listen (port, () => {console.log(`listening on port $ {port}...`)});
---
+++ login.js
@@ -0,0 +1,5 @@
+function login(req, res, next) {
+    console.log("Login...");
+    next();
+    }
+    module.exports = login;
